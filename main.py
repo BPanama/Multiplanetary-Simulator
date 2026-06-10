8c4ea6afcec091a949531de2de7bf010526ddeff
BPanama <20panasiukb@jmf6.org>
2026-06-10 12:44:32 +0100

    Added Vector and Object Classes with tests for each

--- main.py
+++ main.py
@@ -1,16 +1,90 @@
-# This is a sample Python script.
+import math
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Vector class needed for calculations
+class Vector:
+    def __init__(self, x: float = 0, y: float = 0):
+        self.x: float = x
+        self.y: float = y
 
+    # Returns the Vector as a list
+    def GetVector(self) -> list[2]:
+        return [self.x, self.y]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # Returns the magnitude of the vector as a single float
+    def GetMagnitude(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
 
+    # Returns the normalized vector as a vector
+    def GetNormalized(self):
+        magnitude = self.GetMagnitude()
+        return Vector(self.x / magnitude, self.y / magnitude)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Adds 2 Vectors using Vector1 + Vector2
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Takes away 2 Vectors using Vector1 - Vector2
+    def __sub__(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        return Vector(x, y)
+
+# Object Class for all the planets, stars, etc
+class Object:
+    def __init__(self, name, colour, mass, radius, currentPos = Vector(), currentVel = Vector()):
+        self.name: str = name
+        self.colour: str = colour
+        self.mass: float = mass
+        self.radius: float = radius
+        self.currentPos: Vector = currentPos
+        self.currentVel: Vector = currentVel
+        self.force: Vector = Vector()
+        self.nextPos: Vector = Vector()
+        self.nextVel: Vector = Vector()
+
+    # Calculates the forces and gets the next position
+    def CalculateNextPos(self) -> None:
+        return
+
+    # Returns current position as a Vector
+    def GetPos(self) -> Vector:
+        return self.currentPos
+
+    # Returns the current mass as a float
+    def GetMass(self) -> float:
+        return self.mass
+
+    # Updates the currentPos and currentVel of the object
+    def Update(self) -> None:
+        return
+
+# Vector Test
+
+# Tests the non "__x__" functions and initialisation
+vector1 = Vector(3, 4)
+
+print("x:", vector1.x, ", y:", vector1.y)
+print(vector1.GetVector())
+print(vector1.GetMagnitude())
+print(vector1.GetNormalized().GetVector())
+
+# Tests the "__x__" functions
+vector2 = Vector(3, 4)
+
+vector3 = vector1 + vector2
+print(vector3.GetVector())
+vector3 = vector1 - vector2
+print(vector3.GetVector())
+
+# Object Test
+object1 = Object("earth","green",5972000000000000000000000, 6000000)
+print(
+    f"Name: {object1.name}\n"
+    f"Colour: {object1.colour}\n"
+    f"Mass: {object1.mass}\n"
+    f"Radius: {object1.radius}\n"
+    f"Current Position: {object1.currentPos.GetVector()}\n"
+    f"Current Velocity: {object1.currentVel.GetVector()}"
+)
